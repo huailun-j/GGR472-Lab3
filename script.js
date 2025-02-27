578c66ab7a0667d1e6127ac4f623e7972e4f573a
huailun-j <huailun.jiang@mail.utoronto.ca>
2025-02-27 18:17:40 -0500

    added

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const geocoder = new MapboxGeocoder({
 // Append geocoder variable to goeocoder HTML div to position on page
 document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
+// Add event listener which returns map view to full screen on button click using flyTo method
 document.getElementById('returnbutton').addEventListener('click', () => {
     map.flyTo({ center: [-79.39, 43.73], zoom: 10 });
 });
@@ -35,7 +36,7 @@ map.on('load', () => {
     // Add Bike Parking Points Layer
     map.addSource('bike-parking', {
         type: 'geojson',
-        data: 'your-bike-parking-data.geojson' // Replace with actual path
+        data: 'https://huailun-j.github.io/GGR472-Lab3/Data/Bicycle-Parking.geojson' // Replace with actual path
     });
 
     map.addLayer({
@@ -50,12 +51,10 @@ map.on('load', () => {
             ],
             'circle-color': [
                 'step', ['get', 'BICYCLE_CAPACITY'],
-                '#d9f2d9', // 0-20 (lightest green)
-                20, '#a7e0a7',
-                50, '#76cd76',
-                80, '#44bb44',
-                100, '#1faa1f',
-                120, '#0d880d' // >120 (darkest green)
+                '#d9f2d9', // 0-30 (lightest green)
+                60, '#a7e0a7',
+                90, '#44bb44',
+                120, '#0d880d',//>120 (darkest green)
             ],
             'circle-stroke-color': '#000000',
             'circle-stroke-width': 1
@@ -65,7 +64,7 @@ map.on('load', () => {
     // Cycling Network Layer
     map.addSource('cycling-network', {
         type: 'geojson',
-        data: 'https://huailun-j.github.io/GGR472-Lab3/Data/Bicycle-Parking.geojson'
+        data: 'https://huailun-j.github.io/GGR472-Lab3/Data/cycling-network.geojson'
     });
 
     map.addLayer({
@@ -96,7 +95,137 @@ map.on('load', () => {
         }
     });
 
+    // Pop-up windows that appear on a mouse click or hover
+    // pop up, Bicycle Parking. When mouse click, can see the bicycle parking info. Changing cursor on mouse over.
+    map.on('mouseenter', 'bicycle-parking-points', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    
+    // Changing cursor when mouse leave
+    map.on('mouseleave', 'bicycle-parking-points', () => {
+        map.getCanvas().style.cursor = '';
+    });
 
+
+    map.on('click', 'bicycle-parking-points', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(
+                `<b>Bike Parking Capacity:</b> ${e.features[0].properties.BICYCLE_CAPACITY}<br>
+                 <b>Address:</b> ${e.features[0].properties.ADDRESS_FULL}<br>
+                 <b>Parking Type:</b> ${e.features[0].properties.PARKING_TYPE}<br>
+                 <b>Neighbourhood:</b> ${e.features[0].properties.WARD}`
+            )
+            .addTo(map);
+    });
+
+    // pop up, cycling network, mouse enter and mouse leave
+    map.on('mouseenter', 'cycling-neiwork', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'cycling-network', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+    map.on('click', 'cycling-network', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(
+                `<b>Street Name:</b> ${e.features[0].properties.STREET_NAME}<br>
+                 <b>From Street:</b> ${e.features[0].properties.FROM_STREET}
+                 <b>To Street:</b> ${e.features[0].properties.TO_STREET}`
+            )
+            .addTo(map);
+    });
+
+    // create legend here
+    const legendlabels = [
+        '0-30',
+        '30-60',
+        '60-90',
+        '90-120',
+        '>120'
+        
+    ];
+    
+    const legendcolours = [
+        '#d9f2d9',
+        '#a7e0a7',
+        '#76cd76',
+        '#44bb44',
+        '#0d880d'
+    ];
+    
+    //Declare legend  here
+    const legend = document.getElementById('legend');
+    //creating a block for each layer to put the colour and label in
+    legendLabels.forEach((label, i) => {
+        const item = document.createElement('div');
+        const key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = legendColors[i];
+
+        const value = document.createElement('span');
+        value.innerHTML = `${label}`;
+
+        item.appendChild(key); //add the key (colour cirlce) to the legend row
+        item.appendChild(value); //add the value to the legend row
+        legend.appendChild(item); //add row to the legend
+    });
+
+
+    // Add event listener which returns map view to full screen on button click using flyTo method
+    // document.getElementById('returnbutton').addEventListener('click', () => {
+    //     map.flyTo({
+    //         center: [-105, 58],
+    //         zoom: 3,
+    //         essential: true
+    //     });
+    // });
+
+    // Change display of legend based on check box
+    let legendcheck = document.getElementById('legendcheck');
+
+    legendcheck.addEventListener('click', () => {
+        if (legendcheck.checked) {
+            legendcheck.checked = true;
+            legend.style.display = 'block';
+        }
+        else {
+            legend.style.display = "none";
+            legendcheck.checked = false;
+        }
+    });
+
+    // Add event listener to toggle the visibility of the bicycle parking points layer
+    document.getElementById('bicycle4parkingcheck').addEventListener('change', (e) => {
+        map.setLayoutProperty(
+            'bicycle-parking-points',
+            'visibility',
+            e.target.checked ? 'visible' : 'none'
+        );
+    });
+
+    // event listener for cycling network layer
+    document.getElementById('cyclingcheck').addEventListener('change', (e) => {
+        map.setLayoutProperty(
+            'cycle-network',
+            'visibility',
+            e.target.checked ? 'visible' : 'none'
+        );
+    });
+
+    // event listener for neighbourhood layer
+    document.getElementById('neighbourhoodscheck').addEventListener('change', (e) => {
+        map.setLayoutProperty(
+            'neighbourhood-fill',
+            'visibility',
+            e.target.checked ? 'visible' : 'none'
+        );
+    });
+
+});
 
 
 
