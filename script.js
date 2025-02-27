1259c10b02b9b33da3683683b2b16aa1fd061823
huailun-j <huailun.jiang@mail.utoronto.ca>
2025-02-27 00:58:16 -0500

    added

---
+++ script.js
@@ -0,0 +1,10 @@
+//Define access token
+mapboxgl.accessToken = 'pk.eyJ1IjoiaHVhaWx1biIsImEiOiJjbTVvOTJvNzAwZnJrMmtwdGpkMzRvdmk1In0.TnWy4ZzmPCKAX1aYKDWMaQ'; 
+
+const map = new mapboxgl.Map({
+    container: 'my-map', // map container ID
+    style: 'mapbox://styles/huailun/cm7mu0ad8019901qv61wbhg30', // style URL
+    center: [-79.39, 43.73], // starting position [lng, lat], change this for centred map
+    zoom: 10, // starting zoom level, adjust to a suitable size
+});
+
